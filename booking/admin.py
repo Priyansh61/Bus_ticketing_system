e2df0be14e7ba98601231ef59ce31b73921d260c
Priyansh61 <21bec080@iiitdmj.ac.in>
2024-10-24 23:25:28 +0530

    Admin added for trip

--- booking/admin.py
+++ booking/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Bus, Route, Booking, Seat, Order
+from .models import Bus, Route, Booking, Seat, Order, Trip
 
 class BusAdmin(admin.ModelAdmin):
     list_display = ['bus_name', 'bus_number', 'bus_capacity', 'bus_route', 'days_of_operation']
@@ -32,10 +32,17 @@ class OrderAdmin(admin.ModelAdmin):
     list_filter = ['user_id', 'order_email', 'order_phone_number', 'total_price', 'status', 'created_at']
     list_per_page = 10
 
+class TripAdmin(admin.ModelAdmin):
+    list_display = ['bus', 'route', 'date']
+    search_fields = ['bus', 'route', 'date']
+    list_filter = ['bus', 'route', 'date']
+    list_per_page = 10
+
 admin.site.register(Bus, BusAdmin)
 admin.site.register(Route, RouteAdmin)
 admin.site.register(Booking, BookingAdmin)
 admin.site.register(Seat, SeatAdmin)
 admin.site.register(Order, OrderAdmin)
+admin.site.register(Trip, TripAdmin)
 
 # Register your models here.
